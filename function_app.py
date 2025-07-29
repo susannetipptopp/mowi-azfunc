fc4a3d1934228940bf41e57544c341a7907b5692
susannetipptopp <158143327+susannetipptopp@users.noreply.github.com>
2025-07-29 16:08:48 +0200

    Update function_app.py
    Trying for the 1000 time

--- function_app.py
+++ function_app.py
@@ -42,7 +42,6 @@ class DateTimeEncoder(json.JSONEncoder):
 app = func.FunctionApp()
 
 # ===== GetMowiData (full payload) =====
-
 @app.function_name(name="GetMowiData")
 @app.route(route="GetMowiData", auth_level=func.AuthLevel.ANONYMOUS)
 def GetMowiData(req: func.HttpRequest) -> func.HttpResponse:
